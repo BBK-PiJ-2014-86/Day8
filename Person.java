704ee758e47baae77909ef9875d69feb6afcc542
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2014-12-12 07:28:57 +0000

    initial version

---
+++ Person.java
@@ -0,0 +1,18 @@
+package day8;
+
+public class Person {
+	
+	int age;
+	String name;
+	Person next;
+	
+	public Person (int age, String name ){
+		this.age = age;
+		this.name=name;
+		next = null;
+	}
+	
+	public void setNext (Person person) {
+		next = person;
+	}
+}
---
+++ SuperMarket.java
@@ -0,0 +1,38 @@
+package day8;
+
+public class SuperMarket{
+
+
+	
+	public void addPerson(Person person) {
+		
+	}
+
+    public void servePerson() {
+    	
+    }
+    
+    public static void main (String [] args) {
+    	
+    	PersonQueueImplement queue = new PersonQueueImplement ();
+    	
+    	Person person = new Person (20, "John");
+    	Person person1 = new Person (20, "MEryr");
+    	Person person2 = new Person (20, "Kyoko");
+    	
+    	queue.insert(person);
+    	queue.insert(person1);
+    	queue.insert(person2);
+    	
+    	System.out.println(queue.toString());
+    	
+    	queue.retrieve();
+    	queue.retrieve();
+    	
+    	System.out.println(queue.toString());
+    }
+    
+    
+	
+	
+}
