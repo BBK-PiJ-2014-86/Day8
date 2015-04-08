cd477187176a7a1155b7f2de48e07529394918b6
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2015-04-08 11:14:55 +0100

    corrections to insert

--- PersonQueueImplement.java
+++ PersonQueueImplement.java
@@ -6,7 +6,7 @@ public class PersonQueueImplement implements PersonQueue {
 	Person head;
 	
 	public PersonQueueImplement () {
-		head = new Person (0,null);
+		head = null;
 	}
 	
 	@Override
@@ -14,12 +14,14 @@ public class PersonQueueImplement implements PersonQueue {
 		
      	Person current = head; // this creates current which points to where the head points.
 		
-		while (current.next != null) { // checking if next is null - reaching the end of the linked list.
-			current = current.next; // if not null, it will go to the next one pointer
-		}
-		
-		current.setNext(person); // this means next is null and we can now set it to point to next person	
-	
+     	if( current==null) {
+     		head=person;
+     	} else{
+			while (current.next != null) { // checking if next is null - reaching the end of the linked list.
+				current = current.next; // if not null, it will go to the next one pointer
+			}
+			current.setNext(person); // this means next is null and we can now set it to point to next person	
+     	}
 	}
 
 	@Override
