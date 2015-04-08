3faadf58d6b377d897fd58bfd93019c774521338
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2015-04-08 11:14:28 +0100

    setters and getters

--- Person.java
+++ Person.java
@@ -6,6 +6,20 @@ public class Person {
 	String name;
 	Person next;
 	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Person getNext() {
+		return next;
+	}
+
+
+	
 	public Person (int age, String name ){
 		this.age = age;
 		this.name=name;
